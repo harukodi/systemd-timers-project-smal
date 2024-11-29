3c9bc6da649bd02a6e6476620d02ea3d480ef1ab
None <nikolamihov2@gmail.com>
2024-11-29 15:28:09 +0100

    Update restic_backup.sh

--- restic_config/restic_backup.sh
+++ restic_config/restic_backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 BACKUP_PATH="/root/data_folder"
+REMOTE_PATH="/root/remote_backup_folder"
+REMOTE_ADDRESS="10.151.61.33"
+REMOTE_USER="root"
 
 source "$SCRIPT_DIR/.restic_env"
 
@@ -8,3 +11,8 @@ function create_backup {
     restic backup $BACKUP_PATH
 }
 create_backup
+
+function send_backup_to_remote {
+    scp -r $RESTIC_REPOSITORY $REMOTE_USER@$REMOTE_ADDRESS:$REMOTE_PATH
+}
+send_backup_to_remote
