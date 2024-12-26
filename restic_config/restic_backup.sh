9774f4c93d3a2f262bde46c896f6d6e4427f0c3e
xia1997x <xia1997x>
2024-12-26 17:19:27 +0000

    Ändring ut av script kör nu remote repo

--- restic_config/restic_backup.sh
+++ restic_config/restic_backup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-BACKUP_PATH="/root/data_folder"
-REMOTE_PATH="/root/remote_backup_folder"
-REMOTE_ADDRESS="10.151.61.33"
-REMOTE_USER="root"
+BACKUP_PATH="./data_folder"
+REMOTE_PATH="/home/xia1997x-ct/restic-repo"
+REMOTE_ADDRESS="restic-remote"
+REMOTE_USER="xia1997x-ct"
 
 source "$SCRIPT_DIR/.restic_env"
 
 function create_backup {
-    restic backup $BACKUP_PATH
+    restic backup -r sftp:$REMOTE_USER@$REMOTE_ADDRESS:$REMOTE_PATH $BACKUP_PATH
 }
 create_backup
-
-function send_backup_to_remote {
-    scp -r $RESTIC_REPOSITORY $REMOTE_USER@$REMOTE_ADDRESS:$REMOTE_PATH
-}
-send_backup_to_remote
